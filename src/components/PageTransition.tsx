1fe56dc1215af607efbcc6ccf622d8cb0c572d2e
ayeshkadike <ayeshkadike@gmail.com>
2026-07-27 02:04:52 -0400

    inshallah

--- src/components/PageTransition.tsx
+++ src/components/PageTransition.tsx
@@ -13,7 +13,7 @@ export default function PageTransition({ children }: PageTransitionProps) {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: 20 }}
-      transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
+      transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] as const }}
       className="flex-grow flex flex-col pt-32 pb-20"
     >
       {children}
